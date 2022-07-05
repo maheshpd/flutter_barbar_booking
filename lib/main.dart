7fc89269fb9bbc9e18d82ddcec54bc9e6b937a9f
maheshprasad <mahesh223prasad@gmail.com>
2022-07-05 08:52:09 +0530

    init project

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/my_bg.png'),
+            fit: BoxFit.cover
+          ),
+
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton.icon(
+                  onPressed: (){},
+                  icon: const Icon(Icons.phone),
+                  label: const Text('LOGIN WITH PHONE'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black)
+                  ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
